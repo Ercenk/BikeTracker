198b58c361120f673ac540479fd501952433fc2f
Ercenk Keresteci <ercenk@ercenk.com>
2014-07-15 08:50:27 -0700

    tinygpsplus

--- BikeTracker_new_ino/BikeTracker_new_ino.ino
+++ BikeTracker_new_ino/BikeTracker_new_ino.ino
@@ -1,205 +1,98 @@
 // Common
-#include <SPI.h>
 #include <Wire.h>
-#include "Time.h"
-#include "string.h"
+#include <SPI.h>
+#define LOGSERIAL true
 
-// SD
-#include <SD.h>
+// PINS
+#define DO_NOT_USE_TX 7 // GPS TX
+#define DO_NOT_USE_RX 8 // GPS RX
+#define gpsSerial Serial1
+
+#define SD_ChipSelect 10
+#define SD_MOSI 11 // Selected b defult by the SDFat library
+#define SD_MISO 12 // Selected b defult by the SDFat library
+#define SD_SCK 13 // Selected b defult by the SDFat library
+
+#define OLED_DC     6
+#define OLED_CS     4
+#define OLED_RST  5
+
+#define SetupSerialLog() if (LOGSERIAL) Serial.begin(115200)
 
 // GPS
-#include <Adafruit_GPS.h>
+#include <TinyGPS++.h>
+#define PMTK_SET_NMEA_UPDATE_1HZ  F("$PMTK220,1000*1F")
+#define PMTK_SET_NMEA_UPDATE_5HZ  F("$PMTK220,200*2C")
+#define PMTK_SET_NMEA_UPDATE_10HZ F("$PMTK220,100*2F")
 
-// 10DOF
-#include <Adafruit_Sensor.h>
-#include <Adafruit_L3GD20_U.h>
-#include <Adafruit_LSM303_U.h>
-#include <Adafruit_BMP085_U.h>
-#include <Adafruit_10DOF.h>
+
+#define PMTK_SET_BAUD_57600 F("$PMTK251,57600*2C")
+#define PMTK_SET_BAUD_9600 F("$PMTK251,9600*17")
+
+// turn on only the second sentence (GPRMC)
+#define PMTK_SET_NMEA_OUTPUT_RMCONLY F("$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29")
+// turn on GPRMC and GGA
+#define PMTK_SET_NMEA_OUTPUT_RMCGGA F("$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28")
+// turn on ALL THE DATA
+#define PMTK_SET_NMEA_OUTPUT_ALLDATA F("$PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0*28")
+// turn off output
+#define PMTK_SET_NMEA_OUTPUT_OFF F("$PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28")
+
+#define PMTK_LOCUS_STARTLOG  F("$PMTK185,0*22")
+#define PMTK_LOCUS_LOGSTARTED F("$PMTK001,185,3*3C")
+#define PMTK_LOCUS_QUERY_STATUS F("$PMTK183*38")
+#define PMTK_LOCUS_ERASE_FLASH F("$PMTK184,1*22")
+#define LOCUS_OVERLAP 0
+#define LOCUS_FULLSTOP 1
+
+// standby command & boot successful message
+#define PMTK_STANDBY F("$PMTK161,0*28")
+#define PMTK_STANDBY_SUCCESS F("$PMTK001,161,3*36")  // Not needed currently
+#define PMTK_AWAKE F("$PMTK010,002*2D")
+
+// ask for the release and version
+#define PMTK_Q_RELEASE F("$PMTK605*31")
+
+// request for updates on antenna status
+#define PGCMD_ANTENNA F("$PGCMD,33,1*6C")
+#define PGCMD_NOANTENNA F("$PGCMD,33,0*6C")
+
+#define GPSACCURACYFACTOR 7.8
+
+TinyGPSPlus gps;
+
+// SD
+#include <SdFat.h>
+SdFat sd;
+// text file for logging
+ofstream logfile;
 
 // OLED
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+Adafruit_SSD1306              display = Adafruit_SSD1306(OLED_DC, OLED_RST, OLED_CS);
 
-#define LOGSERIAL true
-#define MAXSTRINGLENGTH 256
+// Serial print stream
+ArduinoOutStream serialLog(Serial);
 
-/* Update this with the correct SLP for accurate altitude measurements */
-float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
-#define EARTHRADIUS 637100.9
-#define ONEDEGREEINRADIANS 0.01745329
-#define MILESINNAUTICALMILES 1.15077945
-#define GPSACCURACYFACTOR 7.8
+// store error strings in flash to save RAM
+#define error(s) sd.errorHalt_P(PSTR(s))
 
-// GPS & SD
-#define gpsSerial Serial1
-#define TX 8 // not used
-#define RX 7 // not used
-#define SD_ChipSelect 10
-#define SD_MOSI 11
-#define SD_MISO 12
-#define SD_SCK 13
-#define ledPin 13
 
-#define OLED_DC     6
-#define OLED_CS     4
-#define OLED_RESET  5
+uint32_t timer, logTimer;
+bool timeToLog = false,
+     sdError = true,
+     logFileIsOpen = false,
+     moving = false;
+double currentSpeed, maxSpeed = 0, avgSpeed = 0;
+uint32_t dataCounter = 0;
 
-#define BLE_REQ  3
-#define BLE_RDY  24
-#define BLE_RST  9
+double prevLat = 0, prevLng = 0, distance = 0, lat, lng;
 
-char gpsNoDataString[] = "0/0/0,0:0:0.0,0,0,0,0,0,0,0,0,0,0,";
+uint16_t year;
+byte month, day, hour, minutes, second, hundredths;
 
-Adafruit_GPS GPS(&gpsSerial);
-// 10DOF
-/*
-Adafruit_10DOF                dof   = Adafruit_10DOF();
-Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
-Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
-Adafruit_BMP085_Unified       bmp   = Adafruit_BMP085_Unified(18001);
-Adafruit_L3GD20_Unified       gyro = Adafruit_L3GD20_Unified(20);
-*/
-Adafruit_SSD1306              display = Adafruit_SSD1306(OLED_DC, OLED_RESET, OLED_CS);
-
-File logfile;
-
-char displayBuffer[20];
-
-void error(uint8_t errno)
-{
-  while (1) {
-    uint8_t i;
-    for (i = 0; i < errno; i++) {
-      digitalWrite(ledPin, HIGH);
-      delay(100);
-      digitalWrite(ledPin, LOW);
-      delay(100);
-    }
-    for (i = errno; i < 10; i++) {
-      delay(200);
-    }
-  }
-}
-
-size_t trimwhitespace(char *out, size_t len, const char *str)
-{
-  if (len == 0)
-    return 0;
-
-  const char *end;
-  size_t out_size;
-
-  // Trim leading space
-  while (isspace(*str)) str++;
-
-  if (*str == 0) // All spaces?
-  {
-    *out = 0;
-    return 1;
-  }
-
-  // Trim trailing space
-  end = str + strlen(str) - 1;
-  while (end > str && isspace(*end)) end--;
-  end++;
-
-  // Set output size to minimum of trimmed string length and buffer size minus 1
-  out_size = (end - str) < len - 1 ? (end - str) : len - 1;
-
-  // Copy trimmed string and add null terminator
-  memcpy(out, str, out_size);
-  out[out_size] = 0;
-
-  return out_size;
-}
-
-void setupGps()
-{
-  GPS.begin(9600);
-  gpsSerial.begin(9600);
-
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
-  GPS.sendCommand(PGCMD_ANTENNA);
-
-  delay(1000);
-  gpsSerial.println(PMTK_Q_RELEASE);
-}
-
-/*
-void setupSensors()
-{
-  gyro.enableAutoRange(true);
-
-  if (!gyro.begin())
-  {
-    if (LOGSERIAL) {
-      Serial.println("No L3GD20 detected.");
-    }
-    while (1);
-  }
-
-  if (!accel.begin())
-  {
-    if (LOGSERIAL) {
-      Serial.println("No LSM303 detected.");
-    }
-    while (1);
-  }
-  if (!mag.begin())
-  {
-    if (LOGSERIAL) {
-      Serial.println("No LSM303 detected.");
-    }
-    while (1);
-  }
-  if (!bmp.begin())
-  {
-    if (LOGSERIAL) {
-      Serial.println("No BMP180 detected.");
-    }
-    while (1);
-  }
-}
-*/
-
-void setupSD()
-{
-  if (!SD.begin(SD_ChipSelect, SD_MOSI, SD_MISO, SD_SCK)) {
-    if (LOGSERIAL) {
-      Serial.println("Card init. failed!");
-    }
-    error(2);
-  }
-
-  char filename[13];
-  strcpy(filename, "LOG000.TXT");
-  for (uint8_t i = 0; i < 1000; i++) {
-    filename[3] = '0' + i / 100;
-    filename[4] = '0' + (i % 100) / 10;
-    filename[5] = '0' + (i % 100) % 10;
-    if (! SD.exists(filename)) {
-      break;
-    }
-  }
-
-  logfile = SD.open(filename, FILE_WRITE);
-
-  if ( ! logfile ) {
-    if (LOGSERIAL) {
-      Serial.println("Couldnt create ");
-    }
-    error(3);
-    Serial.print(filename);
-  }
-
-  if (LOGSERIAL) {
-    Serial.println("Writing to ");
-    Serial.print(filename);
-  }
-}
+char logBuffer[256];
 
 void printAt(int16_t x, int16_t y, uint8_t s, char *str)
 {
@@ -210,232 +103,141 @@ void printAt(int16_t x, int16_t y, uint8_t s, char *str)
   display.display();
 }
 
-void setupDisplay()
-{
+void setup() {
+  // Common setup
+  SetupSerialLog();
+
+  // GPS setup
+  gpsSerial.begin(9600);
+  gpsSerial.println(PMTK_SET_NMEA_OUTPUT_ALLDATA);
+  gpsSerial.println(PMTK_SET_NMEA_UPDATE_5HZ);
+  gpsSerial.println(PGCMD_ANTENNA);
+
+  // SD setup
+  if (sd.begin(SD_ChipSelect, SPI_HALF_SPEED)) sdError = false;
+
+  char filename[13];
+  strcpy(filename, "LOG000.TXT");
+  for (uint8_t i = 0; i < 1000; i++) {
+    filename[3] = '0' + i / 100;
+    filename[4] = '0' + (i % 100) / 10;
+    filename[5] = '0' + (i % 100) % 10;
+    if (!sd.exists(filename)) {
+      break;
+    }
+  }
+
+  logfile.open(filename);
+  if (logfile.is_open()) logFileIsOpen = true;
+
+  if (LOGSERIAL) {
+    Serial.print("logging to: "); Serial.println(filename);
+  }
+
   display.begin(SSD1306_SWITCHCAPVCC);
   display.clearDisplay();
-  printAt(0, 8, 2, "ERCENK");
-}
-void getSensorData(char *dofLogBuffer)
-{
+  //Size 2 shows 30 characters, the next ones are cut.
+  // Size 1 shows 147 chars
 
+  char displayBuffer[50];
+  char floatBuffer[6];
 
+  dtostrf((double) (sd.vol()->freeClusterCount() * sd.vol()->blocksPerCluster() / 2) / 1024 / 1024,
+          5, 2, floatBuffer);
+  sprintf(displayBuffer, "%s, %s", filename, floatBuffer);
+  printAt(0, 8, 1, displayBuffer);
 
-  /*
-    display.clearDisplay();
-    sprintf(displayBuffer, "X:%f Y:%f Z:%f", accel_event.acceleration.x, accel_event.acceleration.y, accel_event.acceleration.z);
-    printAt(0, 0, 2, displayBuffer);
-
-    Serial.println(displayBuffer);
-    */
+  delay(1000);
 }
 
-void setup()
-{
-  Serial.begin(115200);
-  setupSD();
-  setupGps();
-  //setupSensors();
-  setupDisplay();
-}
 
-float getDecimalDegree(float nmeaValue) {
-  float minutes;
-  float degrees;
-  float seconds;
-  float milliseconds;
-
-  degrees = trunc(nmeaValue / 100);
-  minutes = nmeaValue - (degrees * 100);
-  seconds = (minutes - trunc(minutes)) * 60;
-  milliseconds = (seconds - trunc(seconds)) * 1000;
-
-  minutes = trunc(minutes);
-  seconds = trunc(seconds);
-
-  return degrees + minutes / 60 + seconds / 3600 + milliseconds / 3600000;
-}
-
-uint32_t timer = millis();
-float previousLatitude = 0, previousLongitude = 0;
-
-char logBuffer[256];
-char gpsLogBuffer[128];
-char dofLogBuffer[128];
-size_t gpsBufferSize = 0;
-bool shouldLog = false;
-float distance;
-
-uint8_t gpsHour;
-uint8_t gpsMinute;
-uint8_t gpsSeconds;
-uint8_t gpsYear;
-uint8_t gpsMonth;
-uint8_t gpsDay;
-uint16_t gpsMilliseconds;
-float latitude;
-float longitude;
-float geoidheight;
-float gpsaltitude;
-float gpsSpeed;
-float angle;
-float magvariation;
-float HDOP;
-char lat;
-char lon;
-char gpsMag;
-boolean fix;
-uint8_t fixquality;
-uint8_t satellites;
-
-// DOF
-sensors_event_t accel_event;
-sensors_event_t mag_event;
-sensors_event_t bmp_event;
-sensors_event_t gyro_event;
-sensors_vec_t   orientation;
-
-float roll, pitch, heading, compHeading, temperature, dofAltitude;
-
-void loop()
-{
-  char c = GPS.read();
-
-  // if a sentence is received, we can check the checksum, parse it...
-  if (GPS.newNMEAreceived()) {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences!
-    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
-    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
-
-    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
-      return;  // we can fail to parse a sentence in which case we should just wait for another
-  }
-
-  // Copy GPS data first
-  gpsHour = GPS.hour;
-  gpsMinute = GPS.minute;
-  gpsSeconds = GPS.seconds;
-  gpsYear = GPS.year;
-  gpsMonth = GPS.month;
-  gpsDay = GPS.day;
-  gpsMilliseconds = GPS.milliseconds;
-  latitude = GPS.latitude;
-  latitude = getDecimalDegree(latitude);
-  longitude = GPS.longitude;
-  geoidheight = GPS.geoidheight;
-  gpsaltitude = GPS.altitude;
-  gpsSpeed = GPS.speed;
-  gpsSpeed *= MILESINNAUTICALMILES;
-  angle = GPS.angle;
-  magvariation = GPS.magvariation;
-  HDOP = GPS.HDOP;
-  lat = GPS.lat;
-  if (lat == 'S')
-    latitude *= -1;
-  lon = GPS.lon;
-  if (lon == 'W')
-    longitude *= -1;
-
-  gpsMag = GPS.mag;
-  fix = GPS.fix;
-  fixquality = GPS.fixquality;
-  satellites = GPS.satellites;
-  /*
-  
-  // DOF
-  accel.getEvent(&accel_event);
-  if (dof.accelGetOrientation(&accel_event, &orientation))
-  {
-    roll = orientation.roll;
-    pitch = orientation.pitch;
-  }
-
-  mag.getEvent(&mag_event);
-  if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation))
-  {
-    heading = orientation.heading;
-  }
-
-  if (dof.magTiltCompensation(SENSOR_AXIS_Z, &mag_event, &accel_event))
-  {
-    if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation))
-    {
-      compHeading = orientation.heading;
-    }
-  }
-
-  bmp.getEvent(&bmp_event);
-  if (bmp_event.pressure)
-  {
-    bmp.getTemperature(&temperature);
-    //altitude = bmp.pressureToAltitude(seaLevelPressure,
-    dofAltitude = bmp.pressureToAltitude(1021,
-                                      bmp_event.pressure,
-                                      temperature);
-  }
-
-  gyro.getEvent(&gyro_event);
-  sprintf(dofLogBuffer, "%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f", roll, pitch, heading, compHeading, temperature, dofAltitude, accel_event.acceleration.x, accel_event.acceleration.y, accel_event.acceleration.z, gyro_event.gyro.x, gyro_event.gyro.y, gyro_event.gyro.z);
-  Serial.println(dofLogBuffer);
-  */
-  
+void loop() {
   if (timer > millis())  timer = millis();
+  if (logTimer > millis())  logTimer = millis();
 
-  if ((millis() - timer) > 2000)
-  {
-    // Are we moving?
-    float deltaLat = latitude - previousLatitude;
-    float deltaLong = longitude - previousLongitude;
-    float meanLatRadians = ((latitude * ONEDEGREEINRADIANS) + (previousLatitude * ONEDEGREEINRADIANS)) / 2;
-
-    previousLatitude = latitude;
-    previousLongitude = longitude;
-
-    distance = EARTHRADIUS * sqrt(pow(deltaLat, 2) + pow(cos(meanLatRadians) * deltaLong, 2));
-    shouldLog = distance > GPSACCURACYFACTOR;
-
-    timer = millis(); // reset the timer
+  // Get data only every other second
+  if ((millis() - timer) <= 2000) {
+    return;
   }
 
-  if (!fix) {
-    if (shouldLog)
+  // Reset the timer
+  timer = millis();
+
+  if (millis() - logTimer > 60000) {
+    timeToLog = true;
+  }
+
+  // define the output buffer
+  obufstream outBuffer(logBuffer, sizeof(logBuffer));
+
+  for (unsigned long start = millis(); millis() - start < 1000;)
+  {
+    while (gpsSerial.available())
     {
-      sprintf(gpsLogBuffer, gpsNoDataString);
-      if (LOGSERIAL) {
-        Serial.println("no fix");
-      }
-    }
-    else
-      return;
-  }
-  else {
-    if (shouldLog) {
-      sprintf(gpsLogBuffer, "%f, %d/%d/%d,%d:%d:%d.%d,%d,%d,%f,%f,%f,%f,%f,%d,%f,%f,",
-              distance, gpsMonth, gpsDay, gpsYear, gpsHour, gpsMinute, gpsSeconds, gpsMilliseconds, fix, fixquality,
-              latitude, longitude, gpsSpeed, angle, gpsaltitude, satellites, HDOP, magvariation);
+      char c = gpsSerial.read();
+      gps.encode(c);
     }
   }
 
-  if (shouldLog)
+  if (gps.location.isUpdated() && gps.location.isValid())
   {
-    gpsBufferSize = trimwhitespace(logBuffer, 128, gpsLogBuffer);
-    gpsBufferSize = trimwhitespace(logBuffer + gpsBufferSize, 128, dofLogBuffer);
+    lat = gps.location.lat();
+    lng = gps.location.lng();
 
+    if (prevLng == 0) {
+      prevLng = lng;
+      prevLat = lat;
+    }
+    distance = gps.distanceBetween(lat, lng, prevLat, prevLng);
+    if (distance > GPSACCURACYFACTOR) {
+      moving = true;
+    }
+
+    if (timeToLog) {
+      if (!moving) {
+        timeToLog = false;
+      }
+      moving = false;
+    }
+
+    prevLng = lng;
+    prevLat = lat;
+
+    dataCounter++;
+    currentSpeed = gps.speed.mph();
+    if (currentSpeed > maxSpeed) {
+      maxSpeed = currentSpeed;
+    }
+    avgSpeed = ((avgSpeed * (dataCounter - 1)) + currentSpeed) / dataCounter;
+
+    char oldFill = outBuffer.fill('0');
+    outBuffer <<
+              setw(2) << int(gps.date.year()) << '-' <<
+              setw(2) << int(gps.date.month()) << '-' <<
+              setw(2) << int(gps.date.day()) << 'T' <<
+              setw(2) << int(gps.time.hour()) << ':' <<
+              setw(2) << int(gps.time.minute()) << ':' <<
+              setw(2) << int(gps.time.second()) << 'Z' ;
+    outBuffer.fill(oldFill);
+
+    outBuffer << ',' <<
+              //dateTimeBuffer << ',' <<
+              setprecision(10) << lat << ',' <<
+              setprecision(10) << lng << ',' <<
+              setprecision(1) << currentSpeed  << ',' <<
+              setprecision(1) << maxSpeed  << ',' <<
+              setprecision(1) << avgSpeed  << ',' <<
+              setprecision(2) << gps.course.deg() << ',' <<
+              setprecision(0) << gps.altitude.feet() << ',' <<
+              gps.satellites.value() << endl;
+
+    double sdSize = (double) (sd.vol()->freeClusterCount() * sd.vol()->blocksPerCluster() / 2) / 1024 / 1024;
     if (LOGSERIAL) {
-      Serial.println(logBuffer);
+      serialLog << logBuffer;
     }
+  }
 
-    char newLine[2] = "\n";
-    uint8_t stringsize = strlen(logBuffer);
-    if (stringsize != logfile.write((uint8_t *)logBuffer, stringsize))
-    {
-      if (LOGSERIAL) {
-        Serial.println("Error writing file");
-      }
-      error(4);
-    }
-    else logfile.write((uint8_t *)newLine, 1);
-    logfile.flush();
+  if (timeToLog) {
+    logfile << logBuffer << flush;
   }
 }
